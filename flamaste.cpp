ea74c7bcf6cba93568b6a1c8a1799513246c4f4e
Tomasz <146115458+inothi@users.noreply.github.com>
2023-11-07 11:08:33 +0100

    Create flamaste.cpp

---
+++ flamaste.cpp
@@ -0,0 +1,44 @@
+/*
+
+###################
+## NOT COMPLETED ##
+###################
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int ilosc_wyrazow;
+    cout << "Podaj ilosc wyrazow (od 1 do 50): ";
+    cin >> ilosc_wyrazow;
+    while ((ilosc_wyrazow > 50) || (ilosc_wyrazow < 1))
+    {
+        cout << "Bledna ilosc wyrazow." << endl;
+        cout << "Podaj ilosc wyrazow (od 1 do 50): ";
+        cin >> ilosc_wyrazow;
+    }
+
+    string *wyrazy = new string[ilosc_wyrazow];
+    for (int i = 0; i < ilosc_wyrazow; i++)
+    {
+        cin >> wyrazy[i];
+    }
+
+    cout << wyrazy[1] << endl;
+    cout << wyrazy[3] << endl;
+
+    delete [] wyrazy;
+    return 0;
+}
+
+/*
+
+###################
+## NOT COMPLETED ##
+###################
+
+*/
