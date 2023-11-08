45b19039b41e019851b8d0fe63748f851b640837
Tomasz <146115458+inothi@users.noreply.github.com>
2023-11-08 12:08:29 +0100

    Update flamaste.cpp

--- flamaste.cpp
+++ flamaste.cpp
@@ -9,7 +9,8 @@ https://pl.spoj.com/problems/FLAMAST
 */
 
 #include <iostream>
-
+#include <string>
+#include <cctype>
 using namespace std;
 
 int main()
@@ -19,21 +20,49 @@ int main()
     cin >> ilosc_wyrazow;
     while ((ilosc_wyrazow > 50) || (ilosc_wyrazow < 1))
     {
-        cout << "Bledna ilosc wyrazow." << endl;
-        cout << "Podaj ilosc wyrazow (od 1 do 50): ";
+        cout << "Podaj poprawna ilosc wyrazow od 1 do 50: ";
         cin >> ilosc_wyrazow;
     }
 
-    string *wyrazy = new string[ilosc_wyrazow];
     for (int i = 0; i < ilosc_wyrazow; i++)
     {
-        cin >> wyrazy[i];
+        string slowo;
+        cin >> slowo;
+        string przechowalnia = "";
+        int licznik = 1, j = 0;
+
+        while (j < slowo.length())
+        {
+            licznik = 1;
+            char litera = slowo[j];
+            j++;
+
+            for (; j < slowo.length() && slowo[j] == litera; j++)
+            {
+                licznik++;
+            }
+
+            if (licznik == 1)
+            {
+                przechowalnia += toupper(litera);
+            }
+
+            else if (licznik == 2)
+            {
+                przechowalnia += toupper(litera);
+                przechowalnia += toupper(litera);
+            }
+
+            else if (licznik > 2)
+            {
+                przechowalnia += toupper(litera);
+                przechowalnia += to_string(licznik);
+            }
+        }
+
+        cout << przechowalnia;
     }
 
-    cout << wyrazy[1] << endl;
-    cout << wyrazy[3] << endl;
-
-    delete [] wyrazy;
     return 0;
 }
 
